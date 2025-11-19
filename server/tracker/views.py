b9a559996ed26a24f244773fb9619851f44b6eb4
Daniel Russell <druss16@gmail.com>
2025-11-19 17:09:13 -0500

    Fixed logout in backend csrf protect

--- server/tracker/views.py
+++ server/tracker/views.py
@@ -3593,6 +3593,7 @@ from django.contrib.auth.models import User, Group
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
+from django.views.decorators.csrf import ensure_csrf_cookie, csrf_protect
 
 from .models import AgentSession  # if you have it; safe to remove if not present
 

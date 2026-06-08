7d1339b00f5e544bd571cca031fe1c72a0f1584a
Daniel Russell <druss16@gmail.com>
2026-06-08 13:36:40 -0400

    temp: disable eventstream (use async classification without websocket) deux

--- server/timeserver/settings.py
+++ server/timeserver/settings.py
@@ -58,7 +58,6 @@ INSTALLED_APPS = [
     "django_celery_results",
     "django_celery_beat",
     "mobile",
-    'django_eventstream',
 ]
 
 SITE_ID = 1
@@ -86,7 +85,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    'django_eventstream.middleware.EventMiddleware',
 ]
 
 # -----------------------------------------------------
